39cfb90c2186037e5f136072a4ee5f4fb76fab8f
belabelaisabela <isabelagualberto7@gmail.com>
2023-10-20 17:14:30 -0400

    update

--- servidor/seeders/20220727021551-adicionar-usarios.js
+++ servidor/seeders/20220727021551-adicionar-usarios.js
@@ -3,7 +3,6 @@ const crypto = require('../servidor/crypto');
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('usuarios', [
-      { nome: 'John Doe', senha: '123' },
       { nome: 'Picolo', senha: '123' },
       { nome: 'Picolo', senha: crypto.encrypt('123')}
     ], {});
